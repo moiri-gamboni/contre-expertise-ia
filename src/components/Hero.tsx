efeab333dcea418c0f9cb5c57c7140d7c718a408
Moiri Gamboni <moiri.gamboni@proton.me>
2024-06-03 10:07:45 +0200

    feat: change hero title and description

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -32,7 +32,7 @@ export function Hero() {
     <header className="overflow-hidden bg-slate-100 lg:bg-transparent lg:px-5">
       <div className="mx-auto grid max-w-6xl grid-cols-1 grid-rows-[auto_1fr] gap-y-16 pt-16 md:pt-20 lg:grid-cols-12 lg:gap-y-20 lg:px-3 lg:pb-36 lg:pt-20 xl:py-32">
         <div className="relative flex items-end lg:col-span-5 lg:row-span-2">
-          <div className="absolute -bottom-12 -top-20 left-0 right-1/2 z-10 rounded-br-6xl bg-blue-600 text-white/10 md:bottom-8 lg:-inset-y-32 lg:left-[-100vw] lg:right-full lg:-mr-40">
+          <div className="absolute -bottom-12 -top-20 left-0 right-1/2 z-10 rounded-br-6xl bg-brand-600 text-white/10 md:bottom-8 lg:-inset-y-32 lg:left-[-100vw] lg:right-full lg:-mr-40">
             <GridPattern
               x="100%"
               y="100%"
@@ -50,18 +50,20 @@ export function Hero() {
         <div className="bg-white pt-16 lg:col-span-7 lg:bg-transparent lg:pl-16 lg:pt-0 xl:pl-20">
           <div className="mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-0">
             <h1 className="font-display text-5xl font-extrabold text-slate-900 sm:text-6xl">
-              Get lost in the world of icon design.
+              Le rapport de la Commission de l'IA est trompeur.
             </h1>
             <p className="mt-4 text-3xl text-slate-600">
-              A book and video course that teaches you how to design your own
-              icons from scratch.
+              Censé guider la stratégie nationale en matière d'IA, il présente
+              de nombreux manquements graves: une ignorance des risques
+              existentiels liés à l'IA, des conflits d'intérêts flagrants, et
+              une capture par des lobbys américains
             </p>
             <div className="mt-8 flex gap-4">
-              <Button href="#free-chapters" color="blue">
-                Get sample chapter
+              <Button href="#free-chapters" color="brand">
+                Lire notre contre-expertise
               </Button>
-              <Button href="#pricing" variant="outline" color="blue">
-                Buy book
+              <Button href="#pricing" variant="outline" color="brand">
+                Assister à notre conférence de presse
               </Button>
             </div>
           </div>
