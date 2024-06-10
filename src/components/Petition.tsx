a7421d5c1df840d3e3c57e22a2f207804e7597b1
imbjdd <b0bb1e@protonmail.com>
2024-06-10 15:43:28 +0200

    feat: add Petition component styling

--- src/components/Petition.tsx
+++ src/components/Petition.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Container } from "./Container";
 import { SectionHeading } from "./SectionHeading";
 
-import petitionBg from '@/images/home-page-01-feature-section-01.jpg'
+import petitionBg from '@/images/cover.png'
 
 export function Petition() {
   return (
@@ -15,41 +15,29 @@ export function Petition() {
         <SectionHeading number="6" id="petition-title">
           Petition
         </SectionHeading>
-        <div className="bg-white">
-          <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
-            <div className="relative overflow-hidden rounded-lg">
-              <div className="absolute inset-0">
-                <Image
-                  src={petitionBg}
-                  fill={true}
-                  alt=""
-                  className="h-full w-full object-cover object-center"
-                />
-              </div>
-              <div className="relative bg-gray-900 bg-opacity-75 px-6 py-32 sm:px-12 sm:py-40 lg:px-16">
-                <div className="relative mx-auto flex max-w-3xl flex-col items-center text-center">
-                  <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                    <span className="block sm:inline">Level up</span>
-                    <span className="block sm:inline">your desk</span>
-                  </h2>
-                  <p className="mt-3 text-xl text-white">
-                    Make your desk beautiful and organized. Post a picture to
-                    social media and watch it get more likes than life-changing
-                    announcements. Reflect on the shallow nature of existence.
-                    At least you have a really nice desk setup.
-                  </p>
-                  <a
-                    href="#"
-                    className="mt-8 block w-full rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
-                  >
-                    Shop Workspace
-                  </a>
-                </div>
+      </Container>
+      <div className="bg-white">
+        <div className="mx-auto py-16 sm:py-24">
+          <div className="relative overflow-hidden">
+            <div className="relative bg-brand-600 px-6 py-32 sm:px-12 sm:py-40 lg:px-16">
+              <div className="relative mx-auto flex max-w-3xl flex-col items-center text-center">
+                <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                  <span className="block sm:inline">Chaque signature est importante</span>
+                </h2>
+                <p className="mt-3 text-xl text-white">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras vehicula, nulla et volutpat tincidunt, ligula nisi facilisis purus, et bibendum risus eros in orci.
+                </p>
+                <a
+                  href="#"
+                  className="mt-8 block w-full rounded-md border border-transparent bg-white px-8 py-3 text-base font-semibold text-gray-900 hover:bg-gray-100 sm:w-auto"
+                >
+                  Demander un nouveau rapport
+                </a>
               </div>
             </div>
           </div>
         </div>
-      </Container>
+      </div>
     </section>
   )
 }
