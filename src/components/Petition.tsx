ae2c2951c91d15a5657a6507b21d478ab09855ae
Moiri Gamboni <moiri.gamboni@proton.me>
2024-06-10 17:35:16 +0200

    style: brand color button

--- src/components/Petition.tsx
+++ src/components/Petition.tsx
@@ -23,12 +23,17 @@ export function Petition() {
             <div className="relative bg-brand-600 px-6 py-32 sm:px-12 sm:py-40 lg:px-16">
               <div className="relative mx-auto flex max-w-3xl flex-col items-center text-left lg:text-center">
                 <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                  <span className="block sm:inline">Chaque signature est importante</span>
+                  <span className="block sm:inline">
+                    Chaque signature est importante
+                  </span>
                 </h2>
-                <p className="mt-3 text-xl text-white tracking-tight">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras vehicula, nulla et volutpat tincidunt, ligula nisi facilisis purus, et bibendum risus eros in orci.
+                <p className="mt-3 text-xl tracking-tight text-white">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vivamus lacinia odio vitae vestibulum vestibulum. Cras
+                  vehicula, nulla et volutpat tincidunt, ligula nisi facilisis
+                  purus, et bibendum risus eros in orci.
                 </p>
-                <Button variant="large" color="slate" className="mt-8">
+                <Button variant="large" color="white" className="mt-8">
                   Demander un nouveau rapport
                 </Button>
               </div>
