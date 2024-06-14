27f133d4c85a99fe528193a4c1e6dc5cdebca3fa
Moiri Gamboni <moiri.gamboni@proton.me>
2024-06-14 12:41:38 +0200

    feat: always display two rows of experts

--- src/components/Experts.tsx
+++ src/components/Experts.tsx
@@ -1,23 +1,34 @@
-import Image, { ImageProps, StaticImageData } from 'next/image'
+'use client'
+
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
 import { SectionHeading } from './SectionHeading'
 import { experts, Person } from '@/contre-expertise/experts'
-import { Disclosure, DisclosureButton, DisclosurePanel, Transition } from '@headlessui/react'
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Transition,
+} from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../tailwind.config'
 
 function Expert(expert: Person) {
   return (
     <>
-      {expert.image ?
+      {expert.image ? (
         <Image
           className="mx-auto h-24 w-24 rounded-full"
           src={expert.image}
           alt={`portrait of ${expert.name}`}
-        /> :
+        />
+      ) : (
         <div className="mx-auto h-24 w-24 rounded-full bg-slate-200" />
-      }
+      )}
       <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">
         {expert.name}
       </h3>
@@ -30,7 +41,7 @@ function ExpertList({ experts }: { experts: Person[] }) {
   return (
     <ul
       role="list"
-      className="mx-auto grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none xl:grid-cols-6"
+      className="mx-auto grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 lg:mx-0 lg:max-w-none xl:grid-cols-6"
     >
       {experts.map((expert) => (
         <li key={expert.name}>
@@ -42,6 +53,36 @@ function ExpertList({ experts }: { experts: Person[] }) {
 }
 
 export default function Experts({ expanded = false }) {
+  const [toDisplay, setToDisplay] = useState(6)
+  
+  // calculate the number of experts to display based on the screen size
+  useEffect(() => {
+    const fullConfig = resolveConfig(tailwindConfig)
+    const updateToDisplay = () => {
+      const width = window.innerWidth
+      if (width >= Number.parseInt(fullConfig.theme.screens.xl.slice(0, -2))) {
+        setToDisplay(12)
+      } else if (
+        width >= Number.parseInt(fullConfig.theme.screens.lg.slice(0, -2))
+      ) {
+        setToDisplay(10)
+      } else if (
+        width >= Number.parseInt(fullConfig.theme.screens.md.slice(0, -2))
+      ) {
+        setToDisplay(8)
+      } else if (
+        width >= Number.parseInt(fullConfig.theme.screens.sm.slice(0, -2))
+      ) {
+        setToDisplay(6)
+      } else {
+        setToDisplay(4)
+      }
+    }
+    updateToDisplay()
+    window.addEventListener('resize', updateToDisplay)
+    return () => window.removeEventListener('resize', updateToDisplay)
+  }, [])
+
   return (
     <section
       id="experts"
@@ -68,8 +109,12 @@ export default function Experts({ expanded = false }) {
       </Container>
       <div className="bg-white pt-24">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <ExpertList experts={experts.slice(0, 6)} />
-          <Disclosure as="div" className="mt-14 flex justify-center" defaultOpen={expanded}>
+          <ExpertList experts={experts.slice(0, toDisplay)} />
+          <Disclosure
+            as="div"
+            className="mt-14 flex justify-center"
+            defaultOpen={expanded}
+          >
             <Transition
               enter="duration-200 ease-out"
               enterFrom="opacity-0 -translate-y-6"
@@ -78,8 +123,8 @@ export default function Experts({ expanded = false }) {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 -translate-y-6"
             >
-              <DisclosurePanel className="origin-top transition w-full">
-                <ExpertList experts={experts.slice(6)} />
+              <DisclosurePanel className="w-full origin-top transition">
+                <ExpertList experts={experts.slice(toDisplay)} />
               </DisclosurePanel>
             </Transition>
             <DisclosureButton className="group h-5">
