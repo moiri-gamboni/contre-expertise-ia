334006da3b5dd48a6645fd6275fc85822d6fbab0
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-06-24 14:12:43 +0200

    refactor: the list of contacts can be configured in Contacts.tsx

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,8 +1,47 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
+
+import PauseaiImage from '@/images/associations/pauseai.svg'
 
 import { Container } from "./Container";
 
-import PauseaiImage from '@/images/associations/pauseai.svg'
+interface ContactItem {
+  img: StaticImageData;
+  imgAlt: string;
+  email: string;
+}
+
+const contacts: Array<ContactItem> = [
+  {
+    img: PauseaiImage,
+    imgAlt: 'PauseAI logo',
+    email: 'hello@example.org',
+  },
+  {
+    img: PauseaiImage,
+    imgAlt: 'PauseAI logo',
+    email: 'hello@example.org',
+  },
+  {
+    img: PauseaiImage,
+    imgAlt: 'PauseAI logo',
+    email: 'hello@example.org',
+  },
+]
+
+
+const OrganisationContact = ({ contact } : { contact: ContactItem}) => {
+  return (
+    <div className="col-span-1 w-full h-full flex flex-col gap-y-8 justify-around items-center">
+      <Image
+        className="max-h-24 object-contain"
+        src={contact.img}
+         alt={contact.imgAlt}
+      />
+    <span className="text-center text-slate-700">{contact.email}</span>
+  </div>
+  )
+};
+
 
 const Contacts = () => {
   return (
@@ -11,38 +50,17 @@ const Contacts = () => {
         <h2 className="font-bold text-center text-3xl sm:text-4xl text-slate-700 tracking-tight">
           Contact the organisations
         </h2>
-        <div className="text-xl mx-auto mt-12 grid max-w-lg grid-cols-1 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:gap-x-10 md:grid-cols-3 lg:mx-0 lg:max-w-none ">
-          <div className="col-span-1 w-full h-full flex flex-col gap-y-8 justify-around items-center">
-            <Image
-              className="max-h-24 object-contain"
-              src={PauseaiImage}
-              alt="Logo PauseAI"
-            />
-            <span className="text-center text-slate-700">hello@example.org</span>
-          </div>
-
-          <div className="col-span-1 w-full h-full flex flex-col gap-y-8 justify-around items-center">
-            <Image
-              className="max-h-24 object-contain"
-              src={PauseaiImage}
-              alt="Logo PauseAI"
-              unoptimized
-            />
-            <span className="text-center text-slate-700">hello@example.org</span>
-          </div>
-
-          <div className="col-span-1 w-full h-full flex flex-col gap-y-8 justify-around items-center">
-            <Image
-                className="max-h-24 object-contain"
-              src={PauseaiImage}
-              alt="Logo PauseAI"
-            />
-            <span className="text-center text-slate-700">hello@example.org</span>
-          </div>
+        <div className="text-xl mx-auto mt-12 grid max-w-lg grid-cols-1 items-center gap-x-10 gap-y-12
+                        sm:max-w-xl
+                        md:grid-cols-3
+                        lg:max-w-none ">
+          {contacts.map((contact, idx) => (
+            <OrganisationContact key={`contact_${idx}`} contact={contact} />
+          ))}
         </div>
       </Container>
     </div>
   )
-}
+};
 
 export default Contacts;
