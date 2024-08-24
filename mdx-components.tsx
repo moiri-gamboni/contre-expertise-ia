f68886dc3a6547dc9a650c1e57593ec2a3d01ca7
Moiri Gamboni <moiri.gamboni@proton.me>
2024-08-24 17:32:17 +0200

    style: allow breaking inside lists

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -25,14 +25,14 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     ul: (props) => (
       <ul
         role="list"
-        className="ml-8 mt-4 list-outside list-disc space-y-3 print:break-inside-avoid"
+        className="ml-8 mt-4 list-outside list-disc space-y-3"
         {...props}
       />
     ),
     ol: (props) => (
       <ul
         role="list"
-        className="ml-8 mt-4 list-outside list-decimal space-y-3 print:break-inside-avoid"
+        className="ml-8 mt-4 list-outside list-decimal space-y-3"
         {...props}
       />
     ),
@@ -40,7 +40,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     a: ({ href = '#', ...props }) => <StyledLink href={href} {...props} />,
     blockquote: (props) => (
       <blockquote
-        className="my-8 border-s-4 border-s-slate-200 ps-4 font-medium italic text-slate-900 [&_p:first-of-type]:before:content-[open-quote] [&_p:last-of-type]:after:content-[close-quote] print:break-inside-avoid"
+        className="my-8 border-s-4 border-s-slate-200 ps-4 font-medium italic text-slate-900 print:break-inside-avoid [&_p:first-of-type]:before:content-[open-quote] [&_p:last-of-type]:after:content-[close-quote]"
         style={{
           quotes: '"\\201C""\\201D""\\2018""\\2019"',
         }}
