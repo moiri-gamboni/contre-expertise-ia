2882b192ca664c2fc088426600035e02906620a6
Moiri Gamboni <moiri.gamboni@proton.me>
2024-09-01 11:49:40 +0200

    style: adjust mobile margins

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -12,8 +12,8 @@ export function Container({
   return (
     <div
       className={clsx(
-        centered ? 'mx-auto' : 'mx-[12.5%]',
-        'px-4 sm:px-6 md:px-0 lg:max-w-[47.5%] print:ml-0 print:max-w-[62.5%] print:p-0',
+        centered ? 'sm:mx-auto' : 'sm:mx-[12.5%]',
+        'px-4 lg:px-0 lg:max-w-[47.5%] print:ml-0 print:max-w-[62.5%] print:p-0',
         className,
       )}
       {...props}
