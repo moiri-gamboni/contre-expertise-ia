5060b85ae183c835bc2ec3a8c343ad52aadab19c
Moiri Gamboni <moiri.gamboni@proton.me>
2024-09-02 16:46:47 +0200

    fix: mobile width

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,7 +7,7 @@ export function Container({
   return (
     <div
       className={clsx(
-        'mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:max-w-4xl lg:px-12 print:ml-0 print:p-0',
+        'w-full mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:max-w-4xl lg:px-12 print:ml-0 print:p-0',
         className,
       )}
       {...props}
