132abc157a43c29186c49f5515021fae05a59580
Moiri Gamboni <moiri.gamboni@proton.me>
2024-09-05 18:13:38 +0200

    fix: remove centered prop

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,7 +5,7 @@ import ResumeContent from '@/contre-expertise/resume.mdx'
 export function Resume() {
   return (
     <section id="resume" aria-label="resume" className="pb-16 pt-20 md:pt-36">
-      <Container centered className="text-lg tracking-tight text-slate-700">
+      <Container className="text-lg tracking-tight text-slate-700">
         <p className="font-display text-4xl font-bold tracking-tight text-slate-900">
           Résumé Exécutif
         </p>
