d190f0d0b9c36db73e36a3eb8849e65ef2cb3346
Moiri Gamboni <moiri.gamboni@proton.me>
2024-09-05 11:11:50 +0200

    feat: remove need for source number

--- src/components/Reference.tsx
+++ src/components/Reference.tsx
@@ -9,8 +9,15 @@ import path from 'path'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 
 interface ReferenceProps {
-  sourceNumbers: number[]
-  children: React.ReactNode
+  children: string
+}
+
+function parseReferenceNumbers(referenceText: string): number[] {
+  const regex = /\[(\d+)/g
+  const matches = [...referenceText.matchAll(regex)].map((match) =>
+    parseInt(match[1], 10),
+  )
+  return [...new Set(matches)]
 }
 
 function getMDXSource(num: number): string {
@@ -24,11 +31,12 @@ function getMDXSource(num: number): string {
   return fs.readFileSync(filePath, 'utf8')
 }
 
-export function Reference({ sourceNumbers, children }: ReferenceProps) {
+export function Reference({ children }: ReferenceProps) {
+  const sourceNumbers = parseReferenceNumbers(children)
   const sources = sourceNumbers.map(getMDXSource)
 
   return (
-    <Tooltip placement="top-start">
+    <Tooltip>
       <TooltipTrigger>{children}</TooltipTrigger>
       <TooltipContent className="w-min rounded-md border border-brand-600 bg-white/80 px-[calc(theme(spacing.4)-1px)] py-[calc(theme(spacing.1)-1px)] text-base font-bold tracking-tight text-brand-700 backdrop-blur-sm focus:outline-none">
         {sources.map((source, index) => (
