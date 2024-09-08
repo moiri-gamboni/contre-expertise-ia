dc9fa72f7455d9debc6f62bbdb4fb80bb0baf170
Moiri Gamboni <moiri.gamboni@proton.me>
2024-09-08 11:07:16 +0200

    refactor: rename intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Footer } from '@/components/Footer'
 import { Hero } from '@/components/Hero'
-import { Resume } from '@/components/Introduction'
+import { Resume } from '@/components/Resume'
 import { NavBar } from '@/components/NavBar'
 import { Report } from '@/components/Report'
 
