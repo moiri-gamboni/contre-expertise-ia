74b06cb84336871921d093f7c8a6721da261fcb5
Moiri Gamboni <moiri.gamboni@proton.me>
2024-09-08 17:36:11 +0200

    style: remove reference underline

--- src/components/Reference.tsx
+++ src/components/Reference.tsx
@@ -47,7 +47,7 @@ export async function Reference({ children }: ReferenceProps) {
   return (
     <Tooltip placement="top-start">
       <TooltipTrigger asChild>
-          <StyledLink href="#">{children}</StyledLink>
+          <StyledLink className='no-underline' href="#">{children}</StyledLink>
       </TooltipTrigger>
       <TooltipContent className="max-w-[30%] rounded-md border border-brand-600 bg-white px-[calc(theme(spacing.4)-1px)] py-[calc(theme(spacing.1)-1px)] tracking-tight text-slate-700 focus:outline-none">
         <MDXRemote source={source} components={{ ...components }} />
