d35612136593cc17c374cfe36dbd7e89bbcb2383
Moiri Gamboni <moiri.gamboni@proton.me>
2024-09-08 11:07:58 +0200

    Allow StyledLink to have JSX children

--- src/components/StyledLink.tsx
+++ src/components/StyledLink.tsx
@@ -1,18 +1,23 @@
 import clsx from "clsx";
 import NextLink, { LinkProps } from "next/link";
+import React from "react";
 
 interface StyledLinkProps extends LinkProps {
-  className?: string
+  className?: string,
+  children: React.ReactNode
 }
 
 export default function StyledLink({
   className,
+  children,
   ...props
 }: StyledLinkProps) {
   return (
     <NextLink
       className={clsx(className, "font-semibold text-brand-700 underline underline-offset-2 hover:text-brand-800 hover:decoration-brand-800 hover:decoration-2 print:break-inside-avoid")}
       {...props}
-    />
+    >
+      {children}
+    </NextLink>
   )
 }
